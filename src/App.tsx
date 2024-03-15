24ae7ba86c7333825577b56901adc1b8de4b0ee8
Petherson Junior <petherson.oliveira@salonline.com.br>
2024-03-15 10:49:48 -0300

    solução da repetição do  accordion

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,25 @@ import { AccordionComponent } from './components/accordion/accordionComponent'
 function App() {
   const faq = [
     {
-      title: "Will Smth",
-      description:"lorem  lorem lorem lorem"
+      title: "Will Smith",
+      subTitulo: "lorem  lorem lorem lorem"
     },
     {
-      title:"Chris Brown",
-      description:"lorem  lorem lçmckçlwknwknvwv"
+      title: "Chris Brown",
+      subTitulo: "lorem  lorem lçmckçlwknwknvwv"
     },
     {
-      title:"Chris Evans",
-      description:"lorem  capitan capitan capitan"
+      title: "Chris Evans",
+      subTitulo: "lorem  capitan capitan capitan"
     }
   ]
   return (
-
     <>
-    <Banner Size='full' 
-    image='./src/assets/cortes-de-cabelo-masculino.webp' 
-    alt='banner cortes cabelos masculinos' 
-    title='lp dos guri'></Banner>
-    {
-      faq.map(function(faqResult) {
-        <AccordionComponent title={faqResult.title}
-        subTitulo={faqResult.description}
-        ></AccordionComponent>
-          
-      })
-    }
-     
+      <Banner Size='full'
+        image='./src/assets/cortes-de-cabelo-masculino.webp'
+        alt='banner cortes cabelos masculinos'
+        title='lp dos guri' />
+      <AccordionComponent contentFaq={faq} />
     </>
   )
 }
--- src/components/accordion/accordionComponent.tsx
+++ src/components/accordion/accordionComponent.tsx
@@ -5,30 +5,37 @@ import {
     AccordionPanel,
     AccordionIcon,
     Box,
-  } from '@chakra-ui/react';
+} from '@chakra-ui/react';
 import React from 'react';
-interface AccordionProps{
-    title?: string;
-    subTitulo?: string;
+interface AccordionProps {
+    contentFaq: accordionGuris[];
 }
+
+export type accordionGuris = {
+    title: string;
+    subTitulo: string;
+};
 
 export const AccordionComponent: React.FC<AccordionProps> = props => {
     return (
         <>
-        <Accordion>
-            <AccordionItem>
-                <h2>
-                <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                        {props.title}
-                    </Box>
-                    <AccordionIcon />
-                </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                    {props.subTitulo}
-                </AccordionPanel>
-            </AccordionItem>
+            <Accordion>
+                {props.contentFaq.map(function (faqResult: any, index: number) {
+                    return <AccordionItem key={index}>
+                        <h2>
+                            <AccordionButton>
+                                <Box as="span" flex='1' textAlign='left'>
+                                    {faqResult.title}
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                            {faqResult.subTitulo}
+                        </AccordionPanel>
+                    </AccordionItem>
+                })}
+
             </Accordion>
         </>
     )
