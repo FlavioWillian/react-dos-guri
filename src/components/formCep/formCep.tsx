e8f269d496c4084ec520d538728a313b74e8e06b
Petherson Junior <petherson.oliveira@salonline.com.br>
2024-03-27 10:56:11 -0300

    renderizando cep

--- src/components/formCep/formCep.tsx
+++ src/components/formCep/formCep.tsx
@@ -1,83 +1,126 @@
-    import {
-    FormLabel,
-    Input,
-    Button,
-    Box
-  } 
+import {
+  FormLabel,
+  Input,
+  Button,
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+}
   from '@chakra-ui/react'
-  import { Form, Formik } from 'formik';
-  import { buscaCep } from '../../services/cep'; 
-  
-  interface FormCepProps{
-     cep?:string
-  }
-  
+import { Form, Formik } from 'formik';
+import { buscaCep } from '../../services/cep';
+import { useState } from 'react';
 
-export const FormCep : React.FC<FormCepProps> = props => {
-    //const { subscribeFormCep, isLoading = false } = props;
-    const isLoading = false;
-    const itemsCep = {
-          cep: '',
-          logradouro:'',
-          complemento: '',
-          bairro:'',
-          localidade:'',
-          uf:'',
-          ibge:'',
-          gia:'',
-          ddd:'',
-          siafi:''
-        };
-    console.log(itemsCep);
-    return (
-        <>
-       <Formik
-            initialValues={props}
-            //validationSchema={props}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onSubmit={(values, actions) => {
-                const responseBuscaCep = buscaCep(values.cep);
-                itemsCep.bairro = responseBuscaCep?.bairro;
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2))
-                  actions.setSubmitting(false)
-                }, 1000)
-                console.log(responseBuscaCep);
-            }}
-            validateOnBlur
-           validateOnChange
-           
-        > 
+interface FormCepProps {
+  cep?: string
+}
+
+
+export const FormCep: React.FC<FormCepProps> = props => {
+  //const { subscribeFormCep, isLoading = false } = props;
+  const isLoading = false;
+  const [fields, setFields] = useState({
+    cep: '',
+    logradouro: '',
+    complemento: '',
+    bairro: '',
+    localidade: '',
+    uf: '',
+    ibge: '',
+    gia: '',
+    ddd: '',
+    siafi: ''
+  });
+  return (
+    <>
+      <Formik
+        initialValues={props}
+        //validationSchema={props}
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        onSubmit={async (values, actions) => {
+          const responseBuscaCep = await buscaCep(values.cep);
+          //console.log(values.cep);
+          console.log(responseBuscaCep);
+          setFields({
+            ...fields, localidade: responseBuscaCep.data.localidade,
+            logradouro: responseBuscaCep.data.logradouro,
+            bairro: responseBuscaCep.data.bairro,
+            uf: responseBuscaCep.data.uf,
+            ibge: responseBuscaCep.data.ibge,
+            gia: responseBuscaCep.data.gia,
+            ddd: responseBuscaCep.data.ddd,
+          })
+        }}
+
+        validateOnBlur
+        validateOnChange
+
+      >
         {({ values, handleChange, handleSubmit, setFieldTouched }) => {
           return (
-        <Form>
-        <FormLabel>CEP</FormLabel>
-        <Input
-          id="cep"
-          inputMode={'text'}
-          value={values?.cep} 
-          placeholder='Digite seu CEP' 
-          onChange={e => { handleChange(e); }}
-          onBlur={() => {
-            setFieldTouched('cep');
-          }}
-          />
-        <Button
-            mt={4}
-            colorScheme='teal'
-            isLoading={isLoading}
-            disabled={!values.cep || isLoading}
-            type='submit'
-            onClick={() => handleSubmit()}
-          >
-            Enviar
-          </Button>
-        </Form>
-        );}}
-        </Formik>
-         <Box>
-           {itemsCep.bairro}
-         </Box>
-        </>
-    );
+            <Form>
+              <FormLabel>CEP</FormLabel>
+              <Input
+                id="cep"
+                inputMode={'text'}
+                value={values?.cep}
+                placeholder='Digite seu CEP'
+                onChange={e => { handleChange(e); }}
+                onBlur={() => {
+                  setFieldTouched('cep');
+                }}
+              />
+              <Button
+                mt={4}
+                colorScheme='teal'
+                isLoading={isLoading}
+                disabled={!values.cep || isLoading}
+                type='submit'
+                onClick={() => handleSubmit()}
+              >
+                Enviar
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
+      <Box>
+        <TableContainer>
+          <Table size='sm'>
+            <Thead>
+              <Tr>
+                <Th>Logradouro</Th>
+                <Th>Localidade</Th>
+                <Th>Bairro</Th>
+                <Th>UF</Th>
+                <Th>IBGE</Th>
+                <Th>GIA</Th>
+                <Th>DDD</Th>
+                <Th>SIAFI</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>{fields.logradouro}</Td>
+                <Td>{fields.localidade}</Td>
+                <Td>{fields.bairro}</Td>
+                <Td>{fields.uf}</Td>
+                <Td>{fields.ibge}</Td>
+                <Td>{fields.gia}</Td>
+                <Td>{fields.ddd}</Td>
+                <Td>{fields.siafi}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box >
+    </>
+  );
 }
--- src/services/cep.ts
+++ src/services/cep.ts
@@ -1,39 +1,10 @@
 import axios from 'axios'
 
-//const api = axios.create();
-interface cepi {
-  cep?: string,
-  logradouro?: string,
-  complemento?: string,
-  bairro?: string,
-  localidade?: string,
-  uf?: string,
-  ibge?: string,
-  gia?: string,
-  ddd?: string,
-  siafi?: string,
-
-}
-export async function buscaCep(cep: string): Promise<cepi[]> {
+export async function buscaCep(cep: string){
   const valueCep = cep;
-  const config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: `https://viacep.com.br/ws/${valueCep}/json/`,
-    headers: { 'Content-Type': 'application/json' }
-  };
 
-
-  axios.request(config)
-      .then((response) => {
-        //  console.log(JSON.stringify(response.data));
-        const data = JSON.stringify(response.data);
-        return data;
-      })
-      .catch((error) => {
-        return console.log(error);
-      })
-
+  return axios.get(`https://viacep.com.br/ws/${valueCep}/json/`) 
+      .catch((error) => console.log(error));
 }
 
 
